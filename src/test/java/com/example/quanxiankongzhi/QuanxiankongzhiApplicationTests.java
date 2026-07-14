1a7f79a477ccc3b9681030904fdb898f6cb2b371
donghua998 <615984811@qq.com>
2026-07-14 13:17:19 +0800

    初始提交

---
+++ src/test/java/com/example/quanxiankongzhi/QuanxiankongzhiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quanxiankongzhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanxiankongzhiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
